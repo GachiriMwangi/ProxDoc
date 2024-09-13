452f4628f908a9b71e15806c468494e98fc54e6a
GachiriMwangi <gachirimwangi24@gmail.com>
2024-09-13 21:31:40 +0300

    Initial Commit.

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+    const [message, setMessage] = useState('');
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+      axios.get('http://192.168.100.29:8000/api/test')
+      .then(response => {
+       // console.log('Success:', response.data);
+        setMessage(response.data.message);
+        setError('');
+      })
+      .catch(error => {
+        console.log('Error details:', error);
+        if (error.response) {        
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log('Error', error.message);
+        }
+        setError(`Failed to fetch data: ${error.message}`);
+      });
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text>Check The App Here!</Text>
+            {error ? <Text style={styles.error}>{error}</Text> : <Text>{message}</Text>}
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    error: {
+        color: 'red',
+    },
 });
+
+export default App;
